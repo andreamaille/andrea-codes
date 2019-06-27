201b2b9e75e14ce947c047970eacd4017f233b8f
andreamaille <andrea.maille@gmail.com>
2019-06-27 12:03:13 -0400

    refactoring code and updated media queries

--- script.js
+++ script.js
@@ -13,17 +13,14 @@ app.headerClick = function () {
         const taglineArray = [
             `and I'm a front-end web developer`,
             `and I've always got snacks`,
-            `and I'm a pretty nice human`,
             `and I'm late night reader`,
+            `and I'm always up for a challenge`,
             `and I'll never turn down coffee`,
-            `and I love a good joke`,
             `and I'm always up for a road trip`,
-            `and I never met a cup of tea I didn't like`,
         ]
-        
         const LineToDisplay = app.random(taglineArray);
-
         $('.tagline').text(LineToDisplay);
+
 
         const headerBackground = document.getElementById("header-background");
         headerBackground.classList.toggle("secondary-header-background");
@@ -43,18 +40,18 @@ app.changeColor = function () {
 
         const smallTriangle = document.getElementById("small-triangle");
         smallTriangle.classList.toggle("secondary-small-triangle");
-        
+
         const guillotineTriangle = document.getElementById("guillotine-triangle");
         guillotineTriangle.classList.toggle("secondary-guillotine-triangle");
     })
-    
+
 }
 
 app.hamburger = function () {
     $('.hamburger').click(function () {
         $(this).toggleClass('active');
         $('nav').toggleClass('show');
-        
+
     });
     // $('.hamburger').on('click', function () {
     //     $('nav').slideToggle('show')
